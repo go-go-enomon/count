5da3790fe5e2f7cb1fb7a07bd16e621d8512ac98
榎本拓馬 <Takuma@enomototakuma-no-MacBook-Pro.local>
2015-06-06 15:57:19 +0900

    first commit

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -9,15 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var count : Int = 0
+    @IBOutlet var label : UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func plus(){
+    
+    count = count + 1
+        
+    label.text = String(count)
+    
     }
 
 
